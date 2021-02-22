3836cad48d8707da385639af11a93a39ffef0f20
Lucas <lucas.almeida@egsys.com.br>
2021-02-22 09:19:30 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/kotlin/com/almeida/hrworker/HrWorkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.almeida.hrworker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
