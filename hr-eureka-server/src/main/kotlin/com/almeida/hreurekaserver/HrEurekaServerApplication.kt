43b71e3905474d337c5f7b554016a1075a9f6201
Lucas <lucas.almeida@egsys.com.br>
2021-02-22 14:18:59 -0300

    Created Eureka-Server

---
+++ hr-eureka-server/src/main/kotlin/com/almeida/hreurekaserver/HrEurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.almeida.hreurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrEurekaServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrEurekaServerApplication>(*args)
+}
