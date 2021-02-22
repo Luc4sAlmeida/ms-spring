dce77acc46557a3c828ff6535c4859b1364e1b4f
Lucas <lucas.almeida@egsys.com.br>
2021-02-22 10:05:50 -0300

    Project hr-payroll Created

---
+++ hr-payroll/src/test/kotlin/com/almeida/hrpayroll/HrPayrollApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.almeida.hrpayroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
