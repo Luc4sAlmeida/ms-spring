4c9910a15a1d0a198b03c310c62d258a91b396f1
Lucas <lucas.almeida@egsys.com.br>
2021-02-22 13:06:52 -0300

    RestTemplate Implemented

---
+++ hr-payroll/src/main/kotlin/com/almeida/hrpayroll/entities/Worker.kt
@@ -0,0 +1,7 @@
+package com.almeida.hrpayroll.entities
+
+data class Worker(
+    var id: Long,
+    var name: String,
+    var dailyIncome: Double
+)
--- hr-payroll/src/main/kotlin/com/almeida/hrpayroll/services/PaymentService.kt
+++ hr-payroll/src/main/kotlin/com/almeida/hrpayroll/services/PaymentService.kt
@@ -1,11 +1,21 @@
 package com.almeida.hrpayroll.services
 
 import com.almeida.hrpayroll.entities.Payment
+import com.almeida.hrpayroll.entities.Worker
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.lang.Exception
 
 @Service
-class PaymentService {
-    fun getPayment(id: Long, days: Int) : Payment {
-        return Payment("Lucas", 100.0, days)
+class PaymentService(val restTemplate: RestTemplate) {
+
+    @Value("\${hr-worker.host}")
+    lateinit var workerHost : String
+
+    fun getPayment(workerId: Long, days: Int) : Payment {
+        val params = mapOf("id" to workerId.toString())
+        val worker = restTemplate.getForObject("$workerHost/workers/{id}", Worker::class.java, params) ?: throw Exception()
+        return Payment(worker.name, worker.dailyIncome, days)
     }
 }
