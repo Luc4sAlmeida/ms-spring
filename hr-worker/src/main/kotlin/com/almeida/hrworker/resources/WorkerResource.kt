08c4235d119698c0b5c67d1aaea712ee9d6fc0c5
Lucas <lucas.almeida@egsys.com.br>
2021-02-23 16:33:25 -0300

    Properities to private repository

--- hr-worker/src/main/kotlin/com/almeida/hrworker/resources/WorkerResource.kt
+++ hr-worker/src/main/kotlin/com/almeida/hrworker/resources/WorkerResource.kt
@@ -5,6 +5,7 @@ import com.almeida.hrworker.repositories.WorkerRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -12,8 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.lang.RuntimeException
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 class WorkerResource(val workerRepository: WorkerRepository) {
